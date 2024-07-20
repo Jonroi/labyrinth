10f0c65839e498d73a1741a5169ba9d31b6a9511
Joni Roine <joni.roine@outlook.com>
2024-07-20 15:42:48 +0300

    code practice

--- Assets/scripts/testCode.cs
+++ Assets/scripts/testCode.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+   
 public class testCode : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{  // Start is called before the first frame update
+
+    private void Start()
     {
-        Debug.Log("Hello World");
-        
+        float A = 2.0f;
+        float B = 3.0f;
+        float C = A * B;
+        float D = A + B + C;
+        C -= 2;
+        A = 5.0f;
+        if (D > (A + C))
+        {
+            B *= 5;
+        }
+        for (int i = 0; i < 10; i++)
+        {
+            A += 1;
+        }
+        void nimi()
+        {
+            Debug.Log("Joni Roine.");
+        }
+        nimi();
+        Debug.Log(A);
+        Debug.Log(B);
+        Debug.Log(C);
+        Debug.Log(D);
     }
 
     // Update is called once per frame
